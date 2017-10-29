8d8f4e6bcce238b50db3e798e091a86ea13bcda2
Kasun Kodagoda <kvkrusl@gmail.com>
2017-10-29 20:48:50 +0530

    #3: Added a simple test for the project

---
+++ Tasks/tests/customTask.spec.ts
@@ -0,0 +1,9 @@
+import * as assert from 'assert';
+
+describe('Custom VSTS Task Tests', () => {
+    beforeEach(() => {});
+
+    it('Should run a simple unit test', () => {
+        assert.equal(true, true, 'Simple test failed');
+    });
+});
